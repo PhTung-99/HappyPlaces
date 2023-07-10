149e008dc5b56d70231fadcaee764b93c2de0225
Tim Pham <tim.pham@techvify.com.vn>
2023-07-10 17:38:03 +0700

    fix

--- app/src/main/java/com/learn/happyplaces/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/learn/happyplaces/AddHappyPlaceActivity.kt
@@ -4,17 +4,12 @@ import android.Manifest
 import android.app.Activity
 import android.app.DatePickerDialog
 import android.content.ActivityNotFoundException
-import android.content.ContentResolver
-import android.content.Context
 import android.content.Intent
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.provider.Settings
-import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.activity.result.ActivityResult
